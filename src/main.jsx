7fb433a28d03e8b2497dbd31bea5dbe4122ad83e
adansyah <adansyah225@gmail.com>
2025-06-19 22:10:39 +0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import App from './App.jsx'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import 'remixicon/fonts/remixicon.css'
+import PreLoader from './components/PreLoader.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <PreLoader />
     <div className='container mx-auto px-4'>
     <Navbar />
     <App />
